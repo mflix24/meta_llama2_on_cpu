05f43b05e7b4ad985d57f8ae9cad04a2c32c0662
mflix24 <banglacourse1223@gmail.com>
2024-03-25 03:00:40 +0600

    codes are written for modification into 2 files.

--- src/helper.py
+++ src/helper.py
@@ -14,12 +14,12 @@ CUSTOM_SYSTEM_PROMPT="You are an advanced assistant that provides translation fr
 
 
 
-# template="""Use the following pieces of information to answer the user's question.
-# If you dont know the answer just say you know, don't try to make up an answer.
+template="""Use the following pieces of information to answer the user's question.
+If you dont know the answer just say you know, don't try to make up an answer.
 
-# Context:{context}
-# Question:{question}
+Context:{context}
+Question:{question}
 
-# Only return the helpful answer below and nothing else
-# Helpful answer
-# """
+Only return the helpful answer below and nothing else
+Helpful answer
+"""
